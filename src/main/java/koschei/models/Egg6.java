a1090b70d4b92fa5f892339856b2bd5f2c1043ca
Nicholas2208 <nikolaj.belyakov.84@gmail>
2022-11-25 14:22:38 +0300

    first

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+@Component
 public class Egg6 {
     private final Needle7 needle;
 
